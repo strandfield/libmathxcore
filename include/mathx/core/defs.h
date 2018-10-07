0601edb4d6f461c628770d81a31e92929bf70486
RugessNome <vincent.chambrin@gmail.com>
2018-10-07 14:49:38 +0200

    add definition of 'limb' type

--- include/mathx/core/defs.h
+++ include/mathx/core/defs.h
@@ -8,4 +8,38 @@
 #  define LIBMATHXCORE_API __declspec(dllimport)
 #endif
 
+
+
+#include <stdint.h>
+
+typedef int mx_ssize_t;
+typedef size_t mx_size_t;
+
+#if LIBMATHXCORE_LIMB_SIZE == 8
+
+typedef uint8_t mx_limb_t;
+#define LIBMATHXCORE_HAS_LONGLIMB_TYPE
+typedef uint16_t mx_longlimb_t;
+
+#elif LIBMATHXCORE_LIMB_SIZE == 16
+
+typedef uint16_t mx_limb_t;
+#define LIBMATHXCORE_HAS_LONGLIMB_TYPE
+typedef uint32_t mx_longlimb_t;
+
+#elif LIBMATHXCORE_LIMB_SIZE == 32
+
+typedef uint32_t mx_limb_t;
+#define LIBMATHXCORE_HAS_LONGLIMB_TYPE
+typedef uint64_t mx_longlimb_t;
+
+#elif LIBMATHXCORE_LIMB_SIZE == 64
+
+typedef uint64_t mx_limb_t;
+
+#else
+# error "libmathxcore : no limb size defined"
+#endif
+
+
 #endif // LIBMATHXCORE_DEFS_H
