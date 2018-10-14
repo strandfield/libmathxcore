f57e7d95ad2bec8c4ccdc30bfdc7b11f0085b923
RugessNome <vincent.chambrin@gmail.com>
2018-10-14 12:37:16 +0200

    fix bug in unsigned integer comparison

--- src/ucomp.c
+++ src/ucomp.c
@@ -18,9 +18,8 @@ mx_ssize_t unbr_comp(const mx_limb_t *a, mx_size_t as, const mx_limb_t *b, mx_si
   mx_ssize_t comp = (as < bs) ? -1 : (as > bs);
   if (comp != 0 || (as == 0 && bs == 0))
     return comp;
-
-  mx_size_t i = as - 1;
-  for (; i >= 0; --i)
+  
+  for (mx_size_t i = as; i-- > 0;)
   {
     comp = (a[i] < b[i]) ? -1 : (a[i] > b[i]);
     if (comp != 0)
